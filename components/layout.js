666c6d50e7a23d43e6e012365f8ee60d257d9c85
LeanLescano <tatilescano11@gmail.com>
2021-10-05 01:02:28 -0300

    Papaparse fix

--- components/layout.js
+++ components/layout.js
@@ -59,6 +59,10 @@ export default function Layout({ children, title = "Finde pastas" }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
+        <script
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js"
+        ></script>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
           rel="stylesheet"
--- pages/menu.js
+++ pages/menu.js
@@ -57,110 +57,102 @@ function Menu() {
   }, [categories]);
 
   return (
-    <>
-      <Head>
-        <script
-          type="text/javascript"
-          src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js"
-        ></script>
-      </Head>
-      <Layout title="Finde pastas | Menú">
-        {!loading ? (
-          <div className="row w-100">
-            <div className="col-12 col-md-6  text-center text-sm-start">
-              <h1 className="fw-bold">Menu - Finde pastas</h1>
-            </div>
-            <div className="col-12 col-md-6 text-center text-sm-end mb-2">
-              <a
-                href="https://wa.me/541131031390?text=Hola!%20Me%20gustar%C3%ADa%20hacer%20un%20pedido..."
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="btn-pedido">
-                  <span className="circle" aria-hidden="true">
-                    <span className="icon arrow"></span>
-                  </span>
-                  <span className="button-text">
-                    <span className="mr-2">Realizar pedido</span>{" "}
-                    <FontAwesomeIcon icon={faWhatsapp} />
-                  </span>
-                </button>
-              </a>
-            </div>
+    <Layout title="Finde pastas | Menú">
+      {!loading ? (
+        <div className="row w-100">
+          <div className="col-12 col-md-6  text-center text-sm-start">
+            <h1 className="fw-bold">Menu - Finde pastas</h1>
           </div>
-        ) : (
-          <h1>Cargando...</h1>
-        )}
-        <div className="row">
-          {categories.map((cat, i) => {
-            return (
-              <div key={i} className="col">
-                <table className="table table-menu">
-                  <thead>
-                    <tr>
-                      <th scope="col fw-bold">{cat.nombre}</th>
-                      {cat.nombre !== "Salsas" && cat.nombre !== "Fideos" ? (
-                        <>
-                          <th scope="col text-center">1/2 doc.</th>
-                          <th scope="col">1 doc.</th>
-                        </>
-                      ) : (
-                        <>
-                          {cat.gramos.map((item, i) => {
-                            return (
-                              <React.Fragment key={i}>
-                                {item && (
-                                  <th scope="col text-center">
-                                    {item >= 1000
-                                      ? item / 1000 + "kg."
-                                      : item + "gr."}
-                                  </th>
-                                )}
-                              </React.Fragment>
-                            );
-                          })}
-                        </>
-                      )}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data
-                      .filter((item) => item.categoria == cat.nombre)
-                      .map((item, i) => {
-                        if (!!item.precio_docena || !!item.precio_media) {
-                          return (
-                            <tr key={i}>
-                              <td>{item.producto}</td>
-                              <td>${item.precio_media}</td>
-                              {item.precio_docena ? (
-                                <td>${item.precio_docena}</td>
-                              ) : (
-                                <td>Consultar</td>
-                              )}
-                            </tr>
-                          );
-                        } else if (!!item.gramos || !!item.gramos2) {
-                          return (
-                            <tr key={i}>
-                              <td>{item.producto}</td>
-                              {item.gramos2 && !item.gramos ? null : (
-                                <td>${item.precio_gramos}</td>
-                              )}
-                              {item.gramos && !item.gramos2 ? null : (
-                                <td>${item.precio_gramos2}</td>
-                              )}
-                            </tr>
-                          );
-                        }
-                      })}
-                  </tbody>
-                </table>
-              </div>
-            );
-          })}
+          <div className="col-12 col-md-6 text-center text-sm-end mb-2">
+            <a
+              href="https://wa.me/541131031390?text=Hola!%20Me%20gustar%C3%ADa%20hacer%20un%20pedido..."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn-pedido">
+                <span className="circle" aria-hidden="true">
+                  <span className="icon arrow"></span>
+                </span>
+                <span className="button-text">
+                  <span className="mr-2">Realizar pedido</span>{" "}
+                  <FontAwesomeIcon icon={faWhatsapp} />
+                </span>
+              </button>
+            </a>
+          </div>
         </div>
-      </Layout>
-    </>
+      ) : (
+        <h1>Cargando...</h1>
+      )}
+      <div className="row">
+        {categories.map((cat, i) => {
+          return (
+            <div key={i} className="col">
+              <table className="table table-menu">
+                <thead>
+                  <tr>
+                    <th scope="col fw-bold">{cat.nombre}</th>
+                    {cat.nombre !== "Salsas" && cat.nombre !== "Fideos" ? (
+                      <>
+                        <th scope="col text-center">1/2 doc.</th>
+                        <th scope="col">1 doc.</th>
+                      </>
+                    ) : (
+                      <>
+                        {cat.gramos.map((item, i) => {
+                          return (
+                            <React.Fragment key={i}>
+                              {item && (
+                                <th scope="col text-center">
+                                  {item >= 1000
+                                    ? item / 1000 + "kg."
+                                    : item + "gr."}
+                                </th>
+                              )}
+                            </React.Fragment>
+                          );
+                        })}
+                      </>
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {data
+                    .filter((item) => item.categoria == cat.nombre)
+                    .map((item, i) => {
+                      if (!!item.precio_docena || !!item.precio_media) {
+                        return (
+                          <tr key={i}>
+                            <td>{item.producto}</td>
+                            <td>${item.precio_media}</td>
+                            {item.precio_docena ? (
+                              <td>${item.precio_docena}</td>
+                            ) : (
+                              <td>Consultar</td>
+                            )}
+                          </tr>
+                        );
+                      } else if (!!item.gramos || !!item.gramos2) {
+                        return (
+                          <tr key={i}>
+                            <td>{item.producto}</td>
+                            {item.gramos2 && !item.gramos ? null : (
+                              <td>${item.precio_gramos}</td>
+                            )}
+                            {item.gramos && !item.gramos2 ? null : (
+                              <td>${item.precio_gramos2}</td>
+                            )}
+                          </tr>
+                        );
+                      }
+                    })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
   );
 }
 
